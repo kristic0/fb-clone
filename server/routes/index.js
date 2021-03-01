08955ee761a3de98feaa3b4c2fb171de1d8d4110
kristic0 <kristic@protonmail.com>
2021-03-01 21:08:28 +0100

    post adding, post reactions

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const verify = require('../helpers/verifyToken.js');
 const { removeUserValidation } = require('../helpers/validation');
 const User = require('../models/User');
+const connection = require('../helpers/db');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -20,6 +21,27 @@ router.post('/admin/removeUser', verify, async (req, res) => {
   if (isDeleted) return res.status(200).send('Successfully deleted!');
 
   return res.status(400).send('Idk wtf');
+});
+
+router.get('/admin/allUsers', async (req, res) => {
+  let users = await User.find({});
+  let allUsers = [];
+  let data = {
+    allUsers: allUsers,
+    count: null,
+  };
+
+  users.forEach((user) => {
+    allUsers.push({
+      id: user._id,
+      name: user.name,
+    });
+  });
+
+  data.allUsers = allUsers;
+  data.count = await connection.db.collection('users').countDocuments();
+
+  res.send(data);
 });
 
 module.exports = router;
