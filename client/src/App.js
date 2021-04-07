b83e1d8e135147c033f12ed268a9021bdf55bc0c
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-04-07 11:55:39 +0200

    Ovo je prva iteracija navbara

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavItem icon="😀" />
+      <NavItem icon=":D" />
+      <NavItem icon=":D" />
+    </Navbar>
   );
+}
+
+function Navbar(props) {
+  return (
+
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+
+}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  );
+
 }
 
 export default App;
