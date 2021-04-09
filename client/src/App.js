e25705291b86ebdaa0da9c4d5a00e603a360bc17
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-04-09 22:11:17 +0200

    Dodate ikonice i transitioni, struktura za dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,56 @@
 
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
+import {ReactComponent as ZvonoIkonica} from './icons/bell.svg';
+import {ReactComponent as MessengerIkonica} from './icons/messenger.svg';
+import {ReactComponent as KaretIkonica} from './icons/caret.svg';
+import {ReactComponent as PlusIkonica} from './icons/plus.svg';
+import {ReactComponent as ZubcanikIkonica} from './icons/cog.svg';
+import {ReactComponent as CevronIkonica} from './icons/chevron.svg';
+import {ReactComponent as StrelicaIkonica} from './icons/arrow.svg';
+import {ReactComponent as MunjaIkonica} from './icons/bolt.svg';
+
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="😀" />
-      <NavItem icon=":D" />
-      <NavItem icon=":D" />
+      <NavItem ikonica={<PlusIkonica/>} />
+      <NavItem ikonica={<ZvonoIkonica/>} />
+      <NavItem ikonica={<MessengerIkonica/>} />
+
+      <NavItem ikonica = {<KaretIkonica/>}>
+        
+        {/* Ovde pisem spustajuci meni*/}
+        <SpustajuciMeni />
+
+
+      </NavItem>
+
     </Navbar>
+  );
+}
+
+function SpustajuciMeni(){
+
+  function SpustajuciItem(props){
+    return(
+      <a href = "#" className = "menu-item">
+
+          {props.children}
+
+      </a>
+    );
+
+
+  }
+
+
+  return(
+    <div className="spustanje">
+
+
+
+    </div>
   );
 }
 
@@ -23,11 +65,15 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+
+  const[open,setOpen] = useState(false);
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
-        {props.icon}
+      <a href="#" className="ikonica-button" onClick = {() => setOpen(!open)}>
+        {props.ikonica}
       </a>
+
+      {open && props.children}
     </li>
   );
 
