bf9ed1ae276235b023c71e904487c13ce24eddd0
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-04-13 23:27:22 +0200

    Deo dropdowna, i skelet za dalje tranzicije[ENG]

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,20 @@ import {ReactComponent as ZubcanikIkonica} from './icons/cog.svg';
 import {ReactComponent as CevronIkonica} from './icons/chevron.svg';
 import {ReactComponent as StrelicaIkonica} from './icons/arrow.svg';
 import {ReactComponent as MunjaIkonica} from './icons/bolt.svg';
+import {CSSTransition} from 'react-transition-group';
 
 
 function App() {
   return (
     <Navbar>
-      <NavItem ikonica={<PlusIkonica/>} />
-      <NavItem ikonica={<ZvonoIkonica/>} />
-      <NavItem ikonica={<MessengerIkonica/>} />
+      <NavItem icon={<PlusIkonica/>} />
+      <NavItem icon={<ZvonoIkonica/>} />
+      <NavItem icon={<MessengerIkonica/>} />
 
-      <NavItem ikonica = {<KaretIkonica/>}>
+      <NavItem icon = {<KaretIkonica/>}>
         
         {/* Ovde pisem spustajuci meni*/}
-        <SpustajuciMeni />
+        <DropdownMenu />
 
 
       </NavItem>
@@ -30,13 +31,16 @@ function App() {
   );
 }
 
-function SpustajuciMeni(){
+function DropdownMenu(){
+  
+  const [activeMenu, setActiveMenu] = useState('main'); // settings i zivotinje
 
-  function SpustajuciItem(props){
+  function DropdownItem(props){
     return(
       <a href = "#" className = "menu-item">
-
+          <span className = "icon-button">{props.leftIcon}</span>
           {props.children}
+          <span className = "icon-right">{props.rightIcon}</span>
 
       </a>
     );
@@ -44,10 +48,26 @@ function SpustajuciMeni(){
 
   }
 
-
   return(
-    <div className="spustanje">
+    <div className="dropdown">
 
+      <CSSTransition
+        in = {activeMenu === 'main'}
+        unmountOnExit
+        timeout={500}
+        classNames = "menu-primary"
+        >
+          <div className = "menu">
+
+      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem 
+        leftIcon = {<ZubcanikIkonica/>}
+        rightIcon = {<CevronIkonica/>}>Settings
+          
+      </DropdownItem>
+    </div>
+      
+      </CSSTransition>
 
 
     </div>
@@ -69,8 +89,8 @@ function NavItem(props) {
   const[open,setOpen] = useState(false);
   return (
     <li className="nav-item">
-      <a href="#" className="ikonica-button" onClick = {() => setOpen(!open)}>
-        {props.ikonica}
+      <a href="#" className="icon-button" onClick = {() => setOpen(!open)}>
+        {props.icon}
       </a>
 
       {open && props.children}
