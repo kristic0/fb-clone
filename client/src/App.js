085204cb68a4540f4791b1c5d18da8269d08d640
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-04-14 21:37:38 +0200

    Skoro zavrsena varzija sa bagom kod drpodowna drugog menija

--- client/src/App.js
+++ client/src/App.js
@@ -34,10 +34,18 @@ function App() {
 function DropdownMenu(){
   
   const [activeMenu, setActiveMenu] = useState('main'); // settings i zivotinje
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el){
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+
 
   function DropdownItem(props){
     return(
-      <a href = "#" className = "menu-item">
+      <a href = "#" className = "menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
           <span className = "icon-button">{props.leftIcon}</span>
           {props.children}
           <span className = "icon-right">{props.rightIcon}</span>
@@ -49,27 +57,51 @@ function DropdownMenu(){
   }
 
   return(
-    <div className="dropdown">
+    <div className="dropdown" style = {{height: menuHeight}}>
 
       <CSSTransition
         in = {activeMenu === 'main'}
         unmountOnExit
         timeout={500}
         classNames = "menu-primary"
+        onEnter = {calcHeight}
         >
           <div className = "menu">
 
       <DropdownItem>My profile</DropdownItem>
       <DropdownItem 
         leftIcon = {<ZubcanikIkonica/>}
-        rightIcon = {<CevronIkonica/>}>Settings
+        rightIcon = {<CevronIkonica/>}
+        goToMenu = "settings"
+        >
+        Settings
           
       </DropdownItem>
     </div>
-      
       </CSSTransition>
 
+      <CSSTransition
+        in = {activeMenu === 'settings'}
+        unmountOnExit
+        timeout={500}
+        classNames = "menu-secondary"
+        >
+          <div className = "menu">
 
+
+      
+      <DropdownItem leftIcon = {<StrelicaIkonica/>}  goToMenu = "main"/>
+        <DropdownItem>Settings</DropdownItem>   
+
+          <DropdownItem>Settings</DropdownItem> 
+          <DropdownItem>Profil</DropdownItem> 
+          <DropdownItem>Druge Opcije</DropdownItem> 
+          <DropdownItem>OKEJ OV JE TEST SAMO</DropdownItem> 
+          <DropdownItem>Settings</DropdownItem> 
+          <DropdownItem>Settings</DropdownItem> 
+          <DropdownItem>Settings</DropdownItem>  
+    </div>
+      </CSSTransition>
     </div>
   );
 }
