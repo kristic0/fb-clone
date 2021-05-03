1164f025f2303f2947551ae47476c44147c75991
rista <aleksaristicnk@gmail.com>
2021-05-03 14:38:52 +0200

    Dodao sam sidebar

---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import './Sidebar.css'
+import SidebarRow from './SidebarRow'
+
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
+import PeopleIcon from '@material-ui/icons/People'
+import ChatIcon from '@material-ui/icons/Chat'
+import StorefrontIcon from '@material-ui/icons/Storefront'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
+import ExpandMoreOutlined from '@material-ui/icons/ExpandLessOutlined'
+
+
+
+const Sidebar = () => {
+   
+
+    return (
+        <div className='sidebar'>
+            <SidebarRow src='https://artprojectsforkids.org/wp-content/uploads/2020/07/Big-Eyes-Post-782x1024.jpg'
+            title='Frankie' />
+            <SidebarRow Icon={LocalHospitalIcon}
+            title='COVID-19 Information Center' />
+
+            <SidebarRow Icon={EmojiFlagsIcon}
+            title='Pages' />
+            <SidebarRow Icon={PeopleIcon}
+            title='Friends' />
+            <SidebarRow Icon={ChatIcon}
+            title='Messenger' />
+            <SidebarRow Icon={StorefrontIcon}
+            title='Marketplace' />
+            <SidebarRow Icon={VideoLibraryIcon}
+            title='Videos' />
+            <SidebarRow Icon={ExpandMoreOutlined}
+            title='More' />
+            
+
+            <SidebarRow />            
+        </div>
+    )
+}
+
+export default Sidebar
