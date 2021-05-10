ca619c9b2bd31e5df71acac4df5282a2ed39e8f9
Rista99 <61054174+Rista99@users.noreply.github.com>
2021-05-10 18:38:22 +0200

    Dodate price i dizajn istih

---
+++ client/src/Components/Feed.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import StoryReel from './StoryReel'
+import MessageSender from './MessageSender'
+import './Feed.css'
+import Post from './Post'
+
+const Feed = () => {
+    return (
+        <div className='feed'>
+            <StoryReel />
+
+
+
+           
+
+        </div>
+    )
+}
+
+export default Feed
