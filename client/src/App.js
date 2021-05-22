85cf3cbc544571a968ba73cadb4232b1bbb4880f
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-05-22 20:04:59 +0200

    Zavrsen navbar sa light temom i ikonicama

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,107 @@
 
-import './App.css';
-import React, {useState} from 'react';
-import {ReactComponent as ZvonoIkonica} from './icons/bell.svg';
-import {ReactComponent as MessengerIkonica} from './icons/messenger.svg';
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search'
+import  HomeIcon  from '@material-ui/icons/Home'
+import  FlagIcon  from '@material-ui/icons/Flag'
+import  SubscriptionsOutlinedIcon  from '@material-ui/icons/SubscriptionsOutlined'
+import  StorefrontOutLinedIcon  from '@material-ui/icons/StorefrontOutlined'
+import  SupervisedUserCircleIcon  from '@material-ui/icons/SupervisedUserCircle'
+import ArrowDropDownRoundedIcon from '@material-ui/icons/ArrowDropDownRounded';
+
+import { Avatar, IconButton } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import {ReactComponent as KaretIkonica} from './icons/caret.svg';
-import {ReactComponent as PlusIkonica} from './icons/plus.svg';
 import {ReactComponent as ZubcanikIkonica} from './icons/cog.svg';
 import {ReactComponent as CevronIkonica} from './icons/chevron.svg';
-import {ReactComponent as StrelicaIkonica} from './icons/arrow.svg';
-import {ReactComponent as MunjaIkonica} from './icons/bolt.svg';
+import { ReactComponent as KaretDole } from './icons/caret-down.svg';
+import {ReactComponent as Katanac} from './icons/154684.svg';
+import {ReactComponent as Upitnik} from './icons/question-mark-svgrepo-com.svg';
+
+
+
 import {CSSTransition} from 'react-transition-group';
+import {useState} from 'react';
 
+import './App.css';
+import './Header.css';
 
-function App() {
-  return (
-    <Navbar>
-      <NavItem icon={<PlusIkonica/>} />
-      <NavItem icon={<ZvonoIkonica/>} />
-      <NavItem icon={<MessengerIkonica/>} />
+const Header = () => {
+    return (
+        <div
+        className='header'>
+            <div className='header__levi'>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png" 
+                alt="Facebook logo"/>
+            </div>
 
-      <NavItem icon = {<KaretIkonica/>}>
-        
-        {/* Ovde pisem spustajuci meni*/}
-        <DropdownMenu />
+            <div className="header__unos">
+                <SearchIcon />
+                <input placeholder='Pretrazi 
+                Fejsbuk' type="text" />
+            </div>
 
+            <div className="header__centar">
+                <div className="header__opcije 
+                header__opcije--aktivno">
+                    <HomeIcon fontsize='large' />
+                </div>
+                <div className="header__opcije">
+                    <FlagIcon fontsize='large' />
+                </div>
+                <div className="header__opcije">
+                    <SubscriptionsOutlinedIcon fontsize='large' />
+                </div>
+                <div className="header__opcije">
+                    <StorefrontOutLinedIcon fontsize='large' />
+                </div>
+                <div className="header__opcije">
+                    <SupervisedUserCircleIcon fontsize='large' />
+                </div>
+            </div>
 
-      </NavItem>
+            <div className="header__desni">
+                    <div className="header__info">
+                        <Avatar />
+                        <h4>Pavle</h4>
 
-    </Navbar>
-  );
+                    </div>
+
+                    <IconButton>
+                        <AddIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ForumIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <NotificationsActiveIcon />
+                    </IconButton>
+
+                    <NavItem icon = {<KaretDole/>}>
+                        <DropdownMenu></DropdownMenu>
+                    </NavItem>
+
+                </div>   
+        </div>   
+    )
+    
+    function NavItem(props) {
+
+      const[open,setOpen] = useState(false);
+      return (
+        <li className="nav-item">
+          <a href="#" className="icon-button" onClick = {() => setOpen(!open)}>
+            {props.icon}
+          </a>
+    
+          {open && props.children}
+        </li>
+      );
 }
-
 function DropdownMenu(){
   
   const [activeMenu, setActiveMenu] = useState('main'); // settings i zivotinje
@@ -41,8 +112,6 @@ function DropdownMenu(){
     setMenuHeight(height);
   }
 
-
-
   function DropdownItem(props){
     return(
       <a href = "#" className = "menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
@@ -52,8 +121,6 @@ function DropdownMenu(){
 
       </a>
     );
-
-
   }
 
   return(
@@ -64,17 +131,16 @@ function DropdownMenu(){
         unmountOnExit
         timeout={500}
         classNames = "menu-primary"
-        onEnter = {calcHeight}
+        
         >
           <div className = "menu">
 
-      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem leftIcon = {<Avatar/>}>Moj Profil</DropdownItem>
       <DropdownItem 
         leftIcon = {<ZubcanikIkonica/>}
         rightIcon = {<CevronIkonica/>}
-        goToMenu = "settings"
-        >
-        Settings
+        goToMenu = "settings">
+        Opcije
           
       </DropdownItem>
     </div>
@@ -88,47 +154,15 @@ function DropdownMenu(){
         >
           <div className = "menu">
 
-
-      
-      <DropdownItem leftIcon = {<StrelicaIkonica/>}  goToMenu = "main"/>
-        <DropdownItem>Settings</DropdownItem>   
-
-          <DropdownItem>Settings</DropdownItem> 
-          <DropdownItem>Profil</DropdownItem> 
-          <DropdownItem>Druge Opcije</DropdownItem> 
-          <DropdownItem>OKEJ OV JE TEST SAMO</DropdownItem> 
-          <DropdownItem>Settings</DropdownItem> 
-          <DropdownItem>Settings</DropdownItem> 
-          <DropdownItem>Settings</DropdownItem>  
+          <DropdownItem leftIcon = {<KaretDole/>}  goToMenu = "main"/>
+          <DropdownItem leftIcon = {<ZubcanikIkonica/>}>Opcije</DropdownItem>   
+          <DropdownItem leftIcon = {<Katanac/>}>Sigurnosne Opcije</DropdownItem> 
+          <DropdownItem leftIcon = {<Upitnik/>}>Pomoc i Podrska</DropdownItem> 
+  
     </div>
       </CSSTransition>
     </div>
   );
+  }
 }
-
-function Navbar(props) {
-  return (
-
-    <nav className="navbar">
-      <ul className="navbar-nav">{props.children}</ul>
-    </nav>
-  );
-
-}
-
-function NavItem(props) {
-
-  const[open,setOpen] = useState(false);
-  return (
-    <li className="nav-item">
-      <a href="#" className="icon-button" onClick = {() => setOpen(!open)}>
-        {props.icon}
-      </a>
-
-      {open && props.children}
-    </li>
-  );
-
-}
-
-export default App;
+export default Header
