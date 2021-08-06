bfd6b5be3907b1c440e6cc7a6299217d60d2a104
Pavle9699 <51297165+Pavle9699@users.noreply.github.com>
2021-08-06 21:56:05 +0200

    Frontend za login

---
+++ client/src/Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
+
+function login() {
+    return (
+        <div className = "login">
+            <img src = "https://cdn.cdnlogo.com/logos/f/23/facebook.svg" alt = "Slika za fejsbuk logo" className = "login__logo"></img>
+            <div className = "login__sadrzaj">
+                <h3>Ulogujte se na Fejsbuk</h3>
+                <form>
+                    <center>
+                        <input type = "email" placeholder = "E-Adresa"/>
+                    </center>
+                    <center>
+                        <input type = "sifra" placeholder = "Lozinka"/>
+                    </center>
+                    <center>
+                        <button type = "submit" className = "login__login">
+                            Prijavite se
+                            </button>
+                    </center>
+                    <center>
+                        <div className = "stranainfo">
+                            <h5>Zaboravili ste lozinku?</h5>
+                            <h5 className = "dot">·</h5>
+                            <Link to = "/Registracija" style  = {{textDecoration: 'none'}}>
+                                <h5 className = "rtd">Napravite novi nalog</h5>
+                            </Link>
+                        </div>        
+                    </center>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default login;
