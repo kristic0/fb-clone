88a8bebebb09721d04a8935090324028a36078cb
Marko Kristic <kristic@protonmail.com>
2021-08-25 19:51:00 +0200

    fixed active friends chat list

--- client/src/Components/PrijateljiTraka/PrijateljiTraka.js
+++ client/src/Components/PrijateljiTraka/PrijateljiTraka.js
@@ -1,40 +1,40 @@
-import React from 'react'
+import React from "react";
 
-import './PrijateljiTraka.css'
-import PrijateljiTrakaRed from './PrijateljiTrakaRed'
-import friendsList from './databaseSimulation.json'
-import axios from 'axios'
-
+import "./PrijateljiTraka.css";
+import PrijateljiTrakaRed from "./PrijateljiTrakaRed";
+import friendsList from "./databaseSimulation.json";
+import axios from "axios";
 
 const PrijateljiTraka = () => {
-    let logovaniKorisnik = JSON.parse(localStorage.getItem("trenutniKorisnik"))
-    
-    // let prijatelji;
-    // const post = () => {
-    
-    //     const user = await axios.get(`/user/getFriend${logovaniKorisnik.friends}`)
-        
-    // console.log(data.friend)
-}
-post();
-      
-    
-    return (
-        <div className='prijateljiTraka'>
-            <p>Prijatelji</p>
-            
+  let logovaniKorisnik = JSON.parse(localStorage.getItem("trenutniKorisnik"));
 
-            
-            {/* {
+  let prijatelji;
+  (async () => {
+    let listaPrijatelja = JSON.parse(
+      localStorage.getItem("trenutniKorisnik")
+    ).friends;
+
+    await Promise.all(
+      listaPrijatelja.map(async (prijateljId) => {
+        const { data } = await axios.get(`/user/getFriend/${prijateljId}`);
+        console.log(data.naslovnaSlika);
+      })
+    );
+  })();
+
+  return (
+    <div className="prijateljiTraka">
+      <p>Prijatelji</p>
+
+      {/* {
             friendsList.map(friend => <PrijateljiTrakaRed src={friend.profilnaSlika} ime={friend.ime + " " +friend.prezime} key={friend.id} />)
             } */}
-            {/* {
+      {/* {
                 prijatelji.map(prijatelj => <PrijateljiTrakaRed src={prijatelj.profilnaSlika} ime={prijatelj.ime + " " + prijatelj.prezime} key={prijatelj.id}/>)
             }
          */}
-                       
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default PrijateljiTraka
+export default PrijateljiTraka;
